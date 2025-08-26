145c45083280bc5159d0c7b63a57d449bbbe2c45
VildanKildeev <vildankildeev@gmail.com>
2025-08-26 09:18:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -308,7 +308,7 @@ async def get_tools_list():
     ]
     return tools_list
 
-app.mount("/", StaticFiles(directory=".", html=True), name="static")
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), reload=True)
