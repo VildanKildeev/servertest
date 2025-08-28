b61fc47bde13cf6c8ed7ca34714be4d010192ce2
VildanKildeev <vildankildeev@gmail.com>
2025-08-28 13:12:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from typing import Optional, List
 from datetime import datetime
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from fastapi.security import OAuth2PasswordBearer
 
 import os
 from dotenv import load_dotenv
@@ -24,6 +25,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 SECRET_KEY = os.environ.get("SECRET_KEY", "your-super-secret-key")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/token")
 
 app = FastAPI(title="СМЗ.РФ API")
 
